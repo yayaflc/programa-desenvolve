f809e09a5713292a3471d4e7876c9a476c29bc98
yaya <yasminccfe@gmail.com>
2023-03-29 04:33:46 -0300

    exemplos da aula 1 da semana 10

---
+++ Semana 10/type-boolean.js
@@ -0,0 +1,12 @@
+// boolean
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const cadastroAtivado = false;
+
+console.log(primeiroNumero === segundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2)
---
+++ Semana 10/type-string.js
@@ -0,0 +1,27 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Leonardo";
+
+// concatenação (+)
+console.log(citacao + meuNome)
+
+// UTF
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+const senha1 = "minhaSenha123"
+console.log(senha1.length) // 13 caracteres
+
+// template string ou template literal
