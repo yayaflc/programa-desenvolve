c0455f0e8baaac5c15f0d6449f15b349245fff4c
yaya <yasminccfe@gmail.com>
2023-03-30 01:36:52 -0300

    add more exemples

---
+++ Semana 10/truthy-falsy.js
@@ -0,0 +1,25 @@
+// boolean
+const usuarioLogado = true
+const contaPaga = false
+
+// truthy ou falsy
+// 0  -> false
+// 1 -> true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// null -> vazio ou nada
+// undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "Alura";
+console.log(typeof numero)
+console.log(typeof texto)
